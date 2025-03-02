5b098ed7762dc340313ff5597942a8c4bc291f4a
Miroslav Tsarev <mtzarev@gmail.com>
2025-03-02 17:10:37 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = ()=>{
     return(
         <>
         <br />
-        <p>All right reserved &copy;</p>
+        <p>All rights reserved &copy;</p>
         <p>Front-end - TMDB API Task</p>
         </>
     );
